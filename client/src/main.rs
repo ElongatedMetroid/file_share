119ce29503e526e0cd8c4d31401e01b84b55e920
Nate <78760239+NateNoNameSOFT@users.noreply.github.com>
2022-08-20 21:00:10 -0700

    Add client framework

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,41 @@
+use std::{net::TcpStream, process, io::{self, Write}};
+
+use retry::{delay::Fixed, retry_with_index};
+
+use file_share::{Command, CommandType};
+
 fn main() {
-    println!("Hello, world!");
+    let mut stream = 
+    // Retry connecting to the server 10 times, once every 1000 milliseconds
+    retry_with_index(Fixed::from_millis(1000).take(9), |current_try| {
+        match TcpStream::connect("127.0.0.1:34254") {
+            Ok(stream) => Ok(stream),
+            Err(error) => {
+                eprintln!("Connection to server failed, try: {current_try}");
+                Err(error)
+            },
+        }
+    });
+
+    let stream = stream.unwrap_or_else(|error| {
+        eprintln!("Failed to connect to server!: {error}");
+        process::exit(1)
+    });
+
+    handle_connection(stream);
+}
+
+fn handle_connection(stream: TcpStream) {
+    println!("Connected to the server!");
+    let mut buf = String::new();
+
+    loop {
+        println!("Enter what you would like to do, run HELP for help");
+
+        io::stdin().read_line(&mut buf).unwrap_or_default();
+
+        let command = Command::parse(buf.as_str());
+    
+        // stream.write_all(command);
+    }
 }
