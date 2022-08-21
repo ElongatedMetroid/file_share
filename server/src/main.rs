f7201e1cb154e64f85f79a263a4cd4d1de1f763d
Nate <78760239+NateNoNameSOFT@users.noreply.github.com>
2022-08-20 21:00:19 -0700

    add server framework

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,9 +1,29 @@
-use std::net::{TcpListener};
+use std::{net::{TcpListener, TcpStream}, io::{Write, Read}};
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:34254").unwrap();
 
     for stream in listener.incoming() {
-        
+        match stream {
+            Ok(stream) => {
+                println!("Client {:?} connected", stream.peer_addr());
+                handle_client(stream);
+            },
+            Err(error) => {
+                eprintln!("Connection to client failed: {error}");
+                continue;
+            }
+        }
     }
+}
+
+fn handle_client(stream: TcpStream) {
+    // Read command `UPLOAD text.txt` `RECIEVE text.txt` `CATALOGUE`
+    // stream.read(buf);
+
+    // parse buf (Syntax checked on client side)
+
+    // let send = match 
+
+    // stream.write_all(send)
 }
