8b6d64c5798937bdbf47a5c18a2667302856824b
Nate <78760239+NateNoNameSOFT@users.noreply.github.com>
2022-08-21 11:10:40 -0700

    Split command into two types, Share which holds the command and command witch only holds data relating to the command. Also add execution of client command method

--- file_share/src/lib.rs
+++ file_share/src/lib.rs
@@ -1,3 +1,5 @@
+use std::{process, fs::File, io::Read};
+
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
@@ -21,6 +23,16 @@ impl CommandType {
             false
         } else {
             true
+        }
+    }
+    /// Returns true if the command runs on the client side
+    pub fn is_client(&self) -> bool {
+        if *self == CommandType::Exit ||
+           *self == CommandType::Help
+        {
+            true
+        } else {
+            false
         }
     }
 }
@@ -66,9 +78,76 @@ impl Command {
             },
         };
 
-        let command = Command { command_type, arg };
+        let command = Command { 
+            command_type, 
+            arg, 
+        };
 
         Ok(command)
     }
-    
+    pub fn command_type(&self) -> &CommandType {
+        &self.command_type
+    }
+    pub fn execute_client_side(&self) -> Result<(), &'static str> {
+        match *self.command_type() {
+            CommandType::Exit => {
+                process::exit(0);
+            }
+            CommandType::Help => {
+                println!(
+                    "{}\n{}\n{}\n{}\n{}",
+                    "----- Help Guide -----",
+                    "EXIT - Exit the client",
+                    "UPLOAD [file] - Upload a file to the server",
+                    "RECIEVE [file] - Recieve a file from the server",
+                    "CATALOGUE - Recieve a list of files from the server",
+                );
+            }
+            _ => return Err("Tried to execute serverside command on client"),
+        }
+
+        Ok(())
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Share {
+    command: Command,
+
+    content_len: Option<usize>,
+
+    /// Contains file data
+    file: Option<Vec<u8>>,
+    /// Contains text data, this is interpretted diferent ways depending on the
+    /// CommandType. This can be file names, the file catalogue, etc.
+    text_data: Option<String>,
+    /// Is None when no error has occured
+    server_error_response: Option<String>,
+}
+
+impl Share {
+    pub fn new(command: Command) -> Share {
+        Share { 
+            command, 
+            content_len: None,
+            file: None,
+            text_data: None, 
+            server_error_response: None 
+        }
+    }
+    pub fn prepare_data(&mut self) -> Result<(), Box<dyn std::error::Error>> {
+        match *self.command.command_type() {
+            // Load file into vector
+            CommandType::Upload => {
+                let mut file = File::open(self.command.arg.as_ref().unwrap())?;
+
+                self.file = Some(Vec::new());
+                file.read_to_end(&mut self.file.as_mut().unwrap())?;
+            },  
+
+            _ => eprintln!("Nothing to prepare"),
+        }
+
+        Ok(())
+    }
 }
