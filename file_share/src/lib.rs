bd5ffc132585b7105d4c538d235cab62c722eee4
Nate <78760239+NateNoNameSOFT@users.noreply.github.com>
2022-08-20 20:59:56 -0700

    Write command parser

---
+++ file_share/src/lib.rs
@@ -0,0 +1,72 @@
+#[derive(Debug, PartialEq)]
+pub enum CommandType {
+    // Runs on client
+    Exit,
+    Help,
+
+    // Runs on server
+    Upload,
+    Recieve,
+    Catalogue,
+}
+
+impl CommandType {
+    fn has_arg(&self) -> bool {
+        if *self == CommandType::Exit ||
+           *self == CommandType::Help ||
+           *self == CommandType::Catalogue 
+        {
+            false
+        } else {
+            true
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Command {
+    command_type: CommandType,
+    arg: Option<String>,
+}
+
+impl Command {
+    pub fn parse(command: &str) -> Result<Command, &'static str> {
+        if command.is_empty() {
+            return Err("Parse error: Empty command");
+        }
+
+        let mut command_tokens = command.split_whitespace();
+
+        let command_type = match command_tokens.next().unwrap() {
+            "EXIT" => CommandType::Exit,
+            "HELP" => CommandType::Help,
+
+            "UPLOAD" => CommandType::Upload,
+            "RECIEVE" => CommandType::Recieve,
+            "CATALOGUE" => CommandType::Catalogue,
+
+            _ => {
+                return Err("Parse error: Unknown command type");
+            }
+        };
+
+        let arg: Option<String> = match command_tokens.next() {
+            Some(arg) => Some(arg.to_string()),
+            // Command does not require an argument
+            None if !command_type.has_arg() => None,
+            // Command requires an argument
+            None if command_type.has_arg() => {
+                return Err("Parse error: No argument provided for command");
+            },
+            // ?
+            None => {
+                return Err("Parse error: Unknown error");
+            },
+        };
+
+        let command = Command { command_type, arg };
+
+        Ok(command)
+    }
+    
+}
