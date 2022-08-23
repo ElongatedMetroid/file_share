2a163a001da459fada0d7da3337e3bccc4b68422
Nate <78760239+NateNoNameSOFT@users.noreply.github.com>
2022-08-22 18:54:33 -0700

    Implement multithreading via thread pool

--- server/src/main.rs
+++ server/src/main.rs
@@ -3,29 +3,39 @@ use std::{net::{TcpListener, TcpStream}, io::Write};
 
 use file_share::{Share, Location};
 
+mod threadpool;
+
+use threadpool::ThreadPool;
+
 fn main() {
+    let pool = ThreadPool::build(5).unwrap();
+
     let listener = TcpListener::bind("127.0.0.1:34254").unwrap();
 
     for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
+        let stream = match stream {
+            Ok(stream) => {
                 println!("Client {:?} connected", stream.peer_addr());
-                handle_client(&mut stream);
+                stream
             },
             Err(error) => {
                 eprintln!("Connection to client failed: {error}");
                 continue;
             }
-        }
+        };
+
+        pool.execute(|| {
+            handle_client(stream)
+        });
     }
 }
 
 /// Only the official client will work for the most part so the server wont have
 /// to handle additional things like making sure your command was correct (this
 /// is checked on the official client)
-fn handle_client(stream: &mut TcpStream) {
+fn handle_client(mut stream: TcpStream) {
     loop {
-        let mut share = match Share::read_from_stream(stream, Location::Server) {
+        let mut share = match Share::read_from_stream(&mut stream, Location::Server) {
             Ok(share) => share,
             Err(error) => {
                 eprintln!("{error}");
@@ -34,11 +44,11 @@ fn handle_client(stream: &mut TcpStream) {
         };
     
         stream.flush().unwrap();
-        
+    
         if let Err(e) = share.execute() {
             share.set_error_response(e);
         };
-
-        share.write_to_stream(stream, Location::Server).unwrap();
+    
+        share.write_to_stream(&mut stream, Location::Server).unwrap();
     }
 }
